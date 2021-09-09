62077377b88affc9d13c5e3d19a9fa4b89486e80
Benjamin von Barner Altenburg <Beal@itu.dk>
2021-09-09 15:03:40 +0200

    just updating

---
+++ ex1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lecture1_as00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
